689deed68627f832e14543bb839ba2d1a6d35c71
Andres Gaibor <androymartin99@gmail.com>
2025-05-18 17:56:05 -0500

    cv

--- cliente-admin/vite.config.ts
+++ cliente-admin/vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ['historiasqueimpactan.andresgaibor.trade']
+    allowedHosts: ['historiasqueimpactan.andresgaibor.trade', 'historiasqueimpactan2.andresgaibor.trade']
   }
 })
